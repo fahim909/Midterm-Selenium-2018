22606a7b50637cd2fd45378a76d33a70913c1a7b
mrahman <rahmanww@gmail.com>
2018-09-09 09:57:16 -0400

    midterm

---
+++ src/math/problems/Fibonacci.java
@@ -0,0 +1,11 @@
+package math.problems;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+         /*
+          Write 40 Fibonacci numbers with java.
+         */
+
+
+    }
+}
---
+++ src/math/problems/FindLowestDifference.java
@@ -0,0 +1,17 @@
+package math.problems;
+
+public class FindLowestDifference {
+    public static void main(String[] args) {
+        /*
+         Implement in java.
+         Read this below two array. Find the lowest difference between the two array cell.
+        */
+        int [] array1 = {30,12,5,9,2,20,33,1};
+        int [] array2 = {18,25,41,47,17,36,14,19};
+
+
+
+
+    }
+
+}
